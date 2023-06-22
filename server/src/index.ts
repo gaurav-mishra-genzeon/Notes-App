adea010805fef04ec669e700512624876d61b692
gaurav-mishra-genzeon <gaurav.mishra@genzeon.com>
2023-06-22 10:47:23 +0530

    added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import express,{ Express, Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 import dotenv from 'dotenv';
 import user from './routes/userRoutes';
 import notes from './routes/notesRoutes';
 
 const app:Express = express();
 require('dotenv').config();
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -18,6 +19,7 @@ const PORT = process.env.PORT || 3002;
 app.get('/', (req:Request, res:Response) => {
   res.send("Welcome");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server started at Port ${PORT}`);
